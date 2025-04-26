8c7210c2b78b6640c27e611c11f5e2657d80deb2
Yudhapra <yudhapra23@gmail.com>
2025-04-27 03:09:59 +0700

    tes

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,58 @@
+<footer id="footer" class="footer light-background">
+
+<div class="footer-top">
+  <div class="container">
+    <div class="row gy-4">
+      <div class="col-lg-5 col-md-12 footer-about">
+        <a href="index.html" class="logo d-flex align-items-center">
+          <span class="sitename">Al - Furqon</span>
+        </a>
+        <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
+        <div class="social-links d-flex mt-4">
+          <a href=""><i class="bi bi-twitter-x"></i></a>
+          <a href=""><i class="bi bi-facebook"></i></a>
+          <a href=""><i class="bi bi-instagram"></i></a>
+          <a href=""><i class="bi bi-linkedin"></i></a>
+        </div>
+      </div>
+
+      <div class="col-lg-2 col-6 footer-links">
+        <h4>Useful Links</h4>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Events us</a></li>
+          <li><a href="#">Charity</a></li>
+        </ul>
+      </div>
+
+      <div class="col-lg-2 col-6 footer-links">
+        <h4> Program </h4>
+        <p>Kuliah Subuh</p>
+        <p>Ngaji Santai</p>
+        <p>Santri Bahagia</p>
+        <p>Sidaq Hafalan</p>
+        <p>Tabligh Akbar</p>
+      </div>
+
+      <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+        <h4>Contact Us</h4>
+        <p>A108 Adam Street</p>
+        <p>New York, NY 535022</p>
+        <p>United States</p>
+        <p class="mt-4"><strong>Phone:</strong> <span>+1 5589 55488 55</span></p>
+        <p><strong>Email:</strong> <span>info@example.com</span></p>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+
+
+<div class="container copyright text-center">
+  <p>© <span>Copyright</span> <strong class="px-1 sitename">Nova</strong> <span>All Rights Reserved</span></p>
+  <div class="credits">
+  </div>
+</div>
+
+</footer>
